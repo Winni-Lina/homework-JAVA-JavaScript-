91d9069efcb38de2e0bfc905efd9998dd138df2d
YuJin Kim <55020976+Winni-Lina@users.noreply.github.com>
2022-03-02 09:31:08 +0900

    Delete homework directory

--- homework/ArrayList08.java
+++
@@ -1,8 +0,0 @@
-package homework;
-
-public class ArrayList08 {
-    private int size = 0;
-    
-    private Object[] elementData = new Object[100];
-
-}
--- homework/MainRunner08.java
+++
@@ -1,9 +0,0 @@
-package homework;
-
-public class MainRunner08{
-    public static void main(String[] args)
-    {
-        ArrayList08 arraylist = new ArrayList08();
-        return ;
-    }
-}
